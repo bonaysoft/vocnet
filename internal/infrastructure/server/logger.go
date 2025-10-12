e3b07069112546aaf98d963acaab0cc56275f853
saltbo <saltbo@foxmail.com>
2025-10-12 11:45:08 +0800

    refactor(server): simplify connect request logging interceptor

--- internal/infrastructure/server/logger.go
+++ internal/infrastructure/server/logger.go
@@ -34,99 +34,94 @@ func Logger() connect.UnaryInterceptorFunc {
 
 			duration := time.Since(start)
 			code := connect.CodeOf(err)
-			level := slog.LevelInfo
-			if err != nil {
-				switch code {
-				case connect.CodeInvalidArgument, connect.CodeFailedPrecondition, connect.CodeNotFound, connect.CodeAlreadyExists, connect.CodePermissionDenied, connect.CodeUnauthenticated:
-					level = slog.LevelWarn
-				default:
-					level = slog.LevelError
-				}
-			}
-
-			attrs := []slog.Attr{
-				slog.String("procedure", req.Spec().Procedure),
-				slog.String("status", code.String()),
-				slog.Duration("duration", duration),
-			}
-
-			if httpMethod := req.HTTPMethod(); httpMethod != "" {
-				attrs = append(attrs, slog.String("http_method", httpMethod))
-			}
-
-			if stream := req.Spec().StreamType.String(); stream != "" {
-				attrs = append(attrs, slog.String("stream", stream))
-			}
-
-			attrs = append(attrs, slog.String("idempotency", req.Spec().IdempotencyLevel.String()))
-
-			if peer := req.Peer(); peer.Addr != "" {
-				attrs = append(attrs, slog.String("peer_addr", peer.Addr))
-			}
-
-			if protocol := req.Peer().Protocol; protocol != "" {
-				attrs = append(attrs, slog.String("protocol", protocol))
-			}
-
-			if q := req.Peer().Query.Encode(); q != "" {
-				attrs = append(attrs, slog.String("query", q))
-			}
-
-			if ua := req.Header().Get("User-Agent"); ua != "" {
-				attrs = append(attrs, slog.String("user_agent", ua))
-			}
-
-			if requestID := req.Header().Get("X-Request-Id"); requestID != "" {
-				attrs = append(attrs, slog.String("request_id", requestID))
-			}
-
-			if forwardedFor := firstForwardedFor(req.Header()); forwardedFor != "" {
-				attrs = append(attrs, slog.String("client_ip", forwardedFor))
-			}
-
-			if contentType := req.Header().Get("Content-Type"); contentType != "" {
-				attrs = append(attrs, slog.String("content_type", contentType))
-			}
-
-			if accept := req.Header().Get("Accept"); accept != "" {
-				attrs = append(attrs, slog.String("accept", accept))
-			}
-
-			if encoding := req.Header().Get("Content-Encoding"); encoding != "" {
-				attrs = append(attrs, slog.String("content_encoding", encoding))
-			}
-
-			if grpcEncoding := req.Header().Get("Grpc-Encoding"); grpcEncoding != "" {
-				attrs = append(attrs, slog.String("grpc_encoding", grpcEncoding))
-			}
-
-			attrs = append(attrs, slog.Int("request_header_count", headerCount(req.Header())))
-
-			if cl := contentLength(req.Header()); cl >= 0 {
-				attrs = append(attrs, slog.Int("request_bytes", cl))
-			}
-
-			if resp != nil {
-				if cl := contentLength(resp.Header()); cl >= 0 {
-					attrs = append(attrs, slog.Int("response_bytes", cl))
-				}
-				if len(resp.Header()) > 0 {
-					attrs = append(attrs, slog.Int("response_header_count", headerCount(resp.Header())))
-				}
-				if len(resp.Trailer()) > 0 {
-					attrs = append(attrs, slog.Int("response_trailer_count", headerCount(resp.Trailer())))
-				}
-			}
-
-			if err != nil {
-				attrs = append(attrs, slog.String("error", err.Error()))
-			}
+			level := determineLogLevel(code, err)
+			attrs := buildLogAttributes(req, resp, code, duration, err)
 
 			logger.LogAttrs(ctx, level, "request completed", attrs...)
 
 			return resp, err
 		}
 	}
+}
+
+func determineLogLevel(code connect.Code, err error) slog.Level {
+	if err == nil {
+		return slog.LevelInfo
+	}
+	switch code {
+	case connect.CodeInvalidArgument, connect.CodeFailedPrecondition, connect.CodeNotFound,
+		connect.CodeAlreadyExists, connect.CodePermissionDenied, connect.CodeUnauthenticated:
+		return slog.LevelWarn
+	default:
+		return slog.LevelError
+	}
+}
+
+func buildLogAttributes(req connect.AnyRequest, resp connect.AnyResponse, code connect.Code, duration time.Duration, err error) []slog.Attr {
+	attrs := requestAttributes(req, code, duration)
+	attrs = append(attrs, responseAttributes(resp)...)
+	if err != nil {
+		attrs = append(attrs, slog.String("error", err.Error()))
+	}
+	return attrs
+}
+
+func requestAttributes(req connect.AnyRequest, code connect.Code, duration time.Duration) []slog.Attr {
+	attrs := []slog.Attr{
+		slog.String("procedure", req.Spec().Procedure),
+		slog.String("status", code.String()),
+		slog.Duration("duration", duration),
+	}
+
+	appendStringAttr(&attrs, "http_method", req.HTTPMethod())
+	appendStringAttr(&attrs, "stream", req.Spec().StreamType.String())
+	appendStringAttr(&attrs, "idempotency", req.Spec().IdempotencyLevel.String())
+
+	peer := req.Peer()
+	appendStringAttr(&attrs, "peer_addr", peer.Addr)
+	appendStringAttr(&attrs, "protocol", peer.Protocol)
+	appendStringAttr(&attrs, "query", peer.Query.Encode())
+
+	header := req.Header()
+	appendStringAttr(&attrs, "user_agent", header.Get("User-Agent"))
+	appendStringAttr(&attrs, "request_id", header.Get("X-Request-Id"))
+	appendStringAttr(&attrs, "client_ip", firstForwardedFor(header))
+	appendStringAttr(&attrs, "content_type", header.Get("Content-Type"))
+	appendStringAttr(&attrs, "accept", header.Get("Accept"))
+	appendStringAttr(&attrs, "content_encoding", header.Get("Content-Encoding"))
+	appendStringAttr(&attrs, "grpc_encoding", header.Get("Grpc-Encoding"))
+
+	attrs = append(attrs, slog.Int("request_header_count", headerCount(header)))
+	if cl := contentLength(header); cl >= 0 {
+		attrs = append(attrs, slog.Int("request_bytes", cl))
+	}
+
+	return attrs
+}
+
+func responseAttributes(resp connect.AnyResponse) []slog.Attr {
+	if resp == nil {
+		return nil
+	}
+
+	attrs := make([]slog.Attr, 0, 3)
+	if cl := contentLength(resp.Header()); cl >= 0 {
+		attrs = append(attrs, slog.Int("response_bytes", cl))
+	}
+	if len(resp.Header()) > 0 {
+		attrs = append(attrs, slog.Int("response_header_count", headerCount(resp.Header())))
+	}
+	if len(resp.Trailer()) > 0 {
+		attrs = append(attrs, slog.Int("response_trailer_count", headerCount(resp.Trailer())))
+	}
+	return attrs
+}
+
+func appendStringAttr(attrs *[]slog.Attr, key, value string) {
+	if value == "" {
+		return
+	}
+	*attrs = append(*attrs, slog.String(key, value))
 }
 
 func firstForwardedFor(header http.Header) string {
